4a756e8dd54a93fd2655327981d99cf3c9765087
mohit darmal <mohitdarmal1907@gmail.com>
2019-04-30 21:02:06 +0530

    changes

--- models/model.js
+++ models/model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const keys = require('../keys');
+const keys = require('../config/keys');
 
 mongoose.connect(keys.mongoURL.connect, {useNewUrlParser:true});
 
