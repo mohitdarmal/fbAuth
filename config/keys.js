0577edb7408cdb20992f8f5d4e018fca3b763226
mohit darmal <mohitdarmal1907@gmail.com>
2019-04-30 21:05:33 +0530

    changes

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+const keys = {
+  fbKeys : {
+      clientID : 'process.env.1222979507870990',
+      clientSecret : 'process.env.f74a045da8d156cd99dcdc077f596b12',
+  },
+
+  cookieKeys : {
+      keys : 'process.env.aksdfjsdakfjsakfjwiersdfasdf',
+  },
+  mongoURL : {
+      connect : 'process.env.mongodb+srv://fbAuth:Mohit@321@fbauth-oympy.mongodb.net/test?retryWrites=true'
+  }
+}
+
+module.exports = keys;
+
