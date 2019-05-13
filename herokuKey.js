7858d7bdfb1d8537a74b41fa4b15123b291e529f
mohit darmal <mohitdarmal1907@gmail.com>
2019-05-13 20:44:07 +0530

    changes

--- herokuKey.js
+++ herokuKey.js
@@ -1,14 +1,14 @@
 const keys = {
   fbKeys : {
-      clientID : process.env.clientID,
-      clientSecret : process.env.clientSecret,
+      clientID : process.env.CLIENT_ID,
+      clientSecret : process.env.CLIENT_SECRET,
   },
 
   cookieKeys : {
-      keys : process.env.cookieKeys,
+      keys : process.env.COOKIE_KEY,
   },
   mongoURL : {
-      connect : process.env.mongoURL,
+      connect : process.env.MONGO_URI,
   }
 }
 
